907ab3cffe68bab84ea9874ebca577aba99a8ca4
thpGitHub <thierrypoupon@hotmail.com>
2019-02-07 22:56:46 +0100

    save du 07022019

--- ArchiReseau.js
+++ ArchiReseau.js
@@ -3,32 +3,89 @@ var form = document.querySelector("form");
 form.addEventListener("submit", function(e){
 
     var champ1=form.elements.champ1.value;
-    console.log(champ1);
-
+    var champ2=form.elements.champ2.value;
+    var champ3=form.elements.champ3.value;
+    var champ4=form.elements.champ4.value;
     var champ5=form.elements.champ5.value;
-    console.log(champ5);
 
     var masqueBinaire="";
     var masqueDecimal="";
+    var adresseReseau="";
 
+    var tab=["0","0","0","0","0","0","0","0",
+             "0","0","0","0","0","0","0","0",
+             "0","0","0","0","0","0","0","0",
+             "0","0","0","0","0","0","0","0"
+            ];
+
+    var tab8bits=[128,64,32,16,8,4,2,1,
+                  128,64,32,16,8,4,2,1,
+                  128,64,32,16,8,4,2,1,
+                  128,64,32,16,8,4,2,1,
+                 ];
+
+
+    //***************************************************************************************************
+    //Champ5 binaire :
+    //on rempli tab avec des "1" en fonction du nombre rentré dans champ5
     for(var i=0;i<champ5;i++){
+        tab[i]="1";
+    }
+
+    //on parcour tab pour remplir la variable masqueBinaire et ajouter des points
+    for (var i=0;i<tab.length;i++){
         if((i===8) || (i===16) || (i===24)){
             masqueBinaire += ".";
+            //console.log(e);
         }
-        masqueBinaire += "1";
-
+        masqueBinaire += tab[i];
     }
-    console.log("masqueBinaire : "+masqueBinaire);
 
+    //insertion de masqueBinaire dans le HTML
     document.getElementById("masqueBinaire").textContent="Masque binaire : "+masqueBinaire;
 
-/* if chiffre >8
-        sinon chiffre boucle for qui parcour tableau 128,64,36...
-        if chiffre >16
-            sinon chiffre boucle for qui parcour tableau 128,64,36...
-                if chiffre >24
-                    sinon chiffre boucle for qui parcour tableau 128,64,36...
- */
+    //****************************************************************************************************
+    // Champ5 Décimal
+    var compteurValeurDecimal=0;
 
-e.preventDefault()
+    for (var i=0;i<=tab.length;i++){
+        if((i===8) || (i===16) || (i===24) || (i===32)){
+            masqueDecimal += compteurValeurDecimal;
+            if(i!=32){masqueDecimal += ".";}//pour ne pas mettre un . à la fin :)
+            compteurValeurDecimal=0;
+
+        }
+        compteurValeurDecimal += tab[i]*tab8bits[i];
+        console.log("compteurValeurDecimal = "+compteurValeurDecimal);
+        console.log("masqueDecimal = "+masqueDecimal);
+    }
+
+    document.getElementById("masqueDecimal").textContent="Masque décimal : "+masqueDecimal;
+
+    //****************************************************************************************************
+    //Adresse Réseau
+
+    var tabReseau=[128,64,32,16,8,4,2,1];
+
+    function addReseau(champ){
+
+        for(var i=0;i<8;i++){
+            if(champ>=tabReseau[i]){
+                champ -=tabReseau[i];
+                adresseReseau += "1";
+            }else(adresseReseau += "0");
+        }
+        adresseReseau+=".";
+
+    }
+
+    addReseau(champ1);
+    addReseau(champ2);
+    addReseau(champ3);
+    addReseau(champ4);
+    document.getElementById("adresseReseau").textContent="Adresse Réseau : "+adresseReseau;
+
+
+    e.preventDefault()
 });
+
---
+++ ArchiReseausav.js
@@ -0,0 +1,34 @@
+var form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+
+    var champ1=form.elements.champ1.value;
+    console.log(champ1);
+
+    var champ5=form.elements.champ5.value;
+    console.log(champ5);
+
+    var masqueBinaire="";
+    var masqueDecimal="";
+
+    for(var i=0;i<champ5;i++){
+        if((i===8) || (i===16) || (i===24)){
+            masqueBinaire += ".";
+        }
+        masqueBinaire += "1";
+
+    }
+    console.log("masqueBinaire : "+masqueBinaire);
+
+    document.getElementById("masqueBinaire").textContent="Masque binaire : "+masqueBinaire;
+
+    /* if chiffre >8
+            sinon chiffre boucle for qui parcour tableau 128,64,36...
+            if chiffre >16
+                sinon chiffre boucle for qui parcour tableau 128,64,36...
+                    if chiffre >24
+                        sinon chiffre boucle for qui parcour tableau 128,64,36...
+     */
+
+    e.preventDefault()
+});
